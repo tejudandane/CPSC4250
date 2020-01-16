17f77ae466e77053e4a403655bcb623c4bbd17fa
Tejas Dandane <tejudandane@gamil.com>
2020-01-16 01:12:26 -0800

    Initial Commit

---
+++ MobileComp_CPSC4250/MobileComp_CPSC4250/MobileComp_CPSC4250/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileComp_CPSC4250.Services;
+using MobileComp_CPSC4250.Views;
+
+namespace MobileComp_CPSC4250
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileComp_CPSC4250/MobileComp_CPSC4250/MobileComp_CPSC4250/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileComp_CPSC4250.Models;
+
+namespace MobileComp_CPSC4250.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
